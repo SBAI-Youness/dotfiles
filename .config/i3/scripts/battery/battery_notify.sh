c97f26af29613401acc98757af73b5fdc3f0387b
Youness SBAI <154528423+SBAI-Youness@users.noreply.github.com>
2026-03-17 19:47:11 -0400

    Create battery_notify.sh for low battery alerts
    Add a script to notify when battery is low

--- .config/i3/scripts/battery/battery_notify.sh
+++ .config/i3/scripts/battery/battery_notify.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+notified=0
+
+while true; do
+    BAT=$(cat /sys/class/power_supply/BAT0/capacity)
+    STATUS=$(cat /sys/class/power_supply/BAT0/status)
+
+    if [ "$BAT" -lt 20 ] && [ "$STATUS" = "Discharging" ]; then
+        if [ $notified -eq 0 ]; then
+            notify-send -u critical "Battery Low" "Battery is at ${BAT}%"
+            notified=1
+        fi
+    else
+        notified=0
+    fi
+
+    sleep 120
+done
